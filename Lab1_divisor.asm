59d98b6749cfd841db9dcb370a9841b70733936c
Lucas-Cappra <108031562+Lucas-Cappra@users.noreply.github.com>
2026-03-16 19:18:46 -0300

    Update Lab1_divisor.asm

--- Lab1_divisor.asm
+++ Lab1_divisor.asm
@@ -27,27 +27,30 @@ dividir:
 	CLR R4 //limpa R4
 	CLR R5 //limpa R5
 	CLR R6 //limpa R6
-	MOV R7,R0 //preserva R0
+	MOV R16, R0 //preserva R0
 
 div_centena:
-	SUB R7,R1
-	BRLO div_dezena //Desvia se R7<R1
+	SUBI R16, 0x64
 	INC R4
-	RJMP div_centena
-
+	CPI R16,0x64
+	BRGE div_centena
+	BRLO div_dezena //Desvia se R7<R1
+	
 div_dezena:
-	SUB R7,R2
-	BRLO div_unidade //Desvia se R7<R2
+	SUBI R16, 0x0A
 	INC R5
-	RJMP div_dezena
+	CPI R16, 0x0A
+	BRGE div_dezena
+	BRLO div_unidade //Desvia se R7<R1
 
 div_unidade:
-	SUB R7,R3
-	BRLO div_sair //Desvia se R7<R3
+	SUBI R16, 0x01
 	INC R6
-	RJMP div_unidade
+	CPI R16, 0x0A
+	BRGE div_unidade
+	BRLO div_sair //Desvia se R7<R1
 
-div_sair:	
+div_sair:
 	MOV R8,R7
 	RET //retorna
 
