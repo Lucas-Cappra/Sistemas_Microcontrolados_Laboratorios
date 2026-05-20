fa70ab5b85f5830f87846fe3db620289db646859
Pedro <105248310+PedroDS4@users.noreply.github.com>
2026-05-19 22:02:35 -0300

    Create filtro_fir_simulation.m

---
+++ Projeto6/Simulacao_Matlab_Filtro_FIR/filtro_fir_simulation.m
@@ -0,0 +1,93 @@
+% Parâmetros
+Fs = 2000;          % Frequência de amostragem (10 kHz)
+t_fim = 2;              % Tamanho da janela temporal
+Ts = 1/Fs;
+% Tempo de -50 ms a 50 ms
+t = 0:Ts:t_fim;
+
+% Mensagens
+f_1 = 10;
+f_2 = 500;
+
+w = sin(2*pi * f_1 * t);
+
+n = sin(2*pi * f_2 * t);
+
+x = w + n;
+
+%Filtro passa-faixa
+
+%Frequencias em Hz do projeto do filtro
+
+Fn = Fs/2;
+
+delta_f = 200;
+
+fs = (f_2 - 10);
+fp = (fs - delta_f);
+
+
+%Normalização das frequências do projeto para a função firpm
+fs_n = fs/Fn;
+fp_n = fp/Fn;
+
+
+f = [0 fp_n fs_n 1];
+a = [1 1 0 0];
+
+M = 16;
+h = firpm (M, f, a);
+
+%Sinal final modulado sem multiplicador
+y = filter(h,1,x);
+
+
+
+% Plot dos Gráficos no Tempo
+
+% Plot do espectro de magnitude
+figure;
+subplot(2, 1, 1);
+plot(t, x, 'red');
+title('Sinal com Ruído Senoidal');
+xlabel('t (s)');
+% ylabel('|W(f)|');
+grid on;
+
+subplot(2,1,2);
+plot(t, y, 'color', 'green', 'linewidth', 4); hold on
+plot(t, w, 'color', 'black', 'linewidth', 2);
+legend('Sinal Filtrado', 'Sinal Original');
+title('Sinal Filtrado');
+xlabel('t (s)');
+% ylabel('|X(f)|');
+grid on;
+
+
+% Transformada de Fourier
+N = length(t);
+f = (-N/2:N/2-1) * (Fs / N); % Frequências
+spectrum_w = abs(fftshift(fft(x)));
+spectrum_y = abs(fftshift(fft(y))); % Espectro de magnitude
+
+% Plot do espectro de magnitude
+figure;
+subplot(2, 1, 1);
+plot(f, spectrum_w,'red');
+title('Espectro de Magnitude do Sinal com Ruído');
+xlabel('Frequência (Hz)');
+ylabel('|W(f)|');
+grid on;
+
+subplot(2,1,2);
+plot(f, spectrum_y,'k');
+title('Espectro de Magnitude do Sinal Filtrado');
+xlabel('Frequência (Hz)');
+ylabel('|X(f)|');
+grid on;
+
+%%
+
+for i = 1:16  
+    disp(h(i));
+end
