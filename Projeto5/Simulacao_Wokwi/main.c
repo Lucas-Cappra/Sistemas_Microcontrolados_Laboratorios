f1ec55ad9e2bf97eb7e42122f1fe1a2d19b3457c
Pedro <105248310+PedroDS4@users.noreply.github.com>
2026-05-19 22:08:54 -0300

    Update main.c

--- Projeto5/Simulacao_Wokwi/main.c
+++ Projeto5/Simulacao_Wokwi/main.c
@@ -9,10 +9,8 @@
 typedef unsigned char byte;
 
 // =============================
-// Protótipos MAX7219
+// ProtÃ³tipos MAX7219
 // =============================
-
-
 void max7219_init(void);
 void max7219_clear(void);
 void exibirCobra(uint8_t x[], uint8_t y[], byte matriz[]);
@@ -30,70 +28,70 @@ uint8_t colunas = 8;
 
 void animacao_game_over(){
 
-	unsigned char dado = 255;
+    unsigned char dado = 255;
 
-	for (uint8_t l = 0; l<8; l++){
-		max7219_clear();
-		max7219_send(l + 1, dado); // i+1 porque os registradores do MAX vão de 0x01 a 0x08
-		_delay_ms(40);
-		
+    for (uint8_t l = 0; l<8; l++){
+        max7219_clear();
+        max7219_send(l + 1, dado); // i+1 porque os registradores do MAX vÃ£o de 0x01 a 0x08
+        _delay_ms(40);
+       
 
-	}
+}
 
 }
 
 
 
 void setup_hardware(){
-	// Setup
-	// Pinos PC1, PC2, PC3 como entrada (Botões)
-	DDRD &= ~((1 << DDD7) | (1 << DDD6) | (1 << DDD5) | (1 << DDD4) | (1 << DDD3) | (1 << DDD2));
-	DDRB &= ~( (1 << DDB5) | (1 << DDB3) | (1 << DDB2) );
+      // Setup
+    // Pinos PC1, PC2, PC3 como entrada (BotÃµes)
+    DDRD &= ~((1 << DDD7) | (1 << DDD6) | (1 << DDD5) | (1 << DDD4) | (1 << DDD3) | (1 << DDD2));
+    DDRB &= ~( (1 << DDB5) | (1 << DDB3) | (1 << DDB2) );
 
 }
 
 int main() {
-	// Definindo o tamanho da matriz (8x8 como no seu projeto)
+    // Definindo o tamanho da matriz (8x8 como no seu projeto)
 
-	setup_hardware();
-	setup_adc();
-
-
-	Snake cobra;
-
-	byte matriz[8];
-
-	lcd_init();
-
-	max7219_init();
-	max7219_clear();
+    setup_hardware();
+    setup_adc();
 
 
-	uint8_t count_minutes = 0;
-	Iniciar_cobra(&cobra);
-	
-	
-	while(1){
-		
-		controlar_direcao(&cobra);
-		Mover_cobra(&cobra);
+    Snake cobra;
 
-		lcd_exibir_coordenadas(cobra.x, cobra.y);
-		exibirCobra(cobra.x, cobra.y, matriz);
+    byte matriz[8];
 
-		uint8_t time_frame = cobra.velocidade;
-		_delay_ms(500);
+    lcd_init();
 
-		count_minutes++;
-		if (count_minutes == 20 && cobra.tamanho<8){
-			cobra.velocidade = cobra.velocidade - 50;
-			aumentar_cobra(&cobra);
-			count_minutes = 0;
-		}
+    max7219_init();
+    max7219_clear();
 
-		verifica_limites(&cobra, &count_minutes);
 
-	}
+    uint8_t count_minutes = 0;
+    Iniciar_cobra(&cobra);
+  
+    
+    while(1){
+      
+        controlar_direcao(&cobra);
+        Mover_cobra(&cobra);
 
-	return 0;
+        lcd_exibir_coordenadas(cobra.x, cobra.y);
+        exibirCobra(cobra.x, cobra.y, matriz);
+
+        uint8_t time_frame = cobra.velocidade;
+        _delay_ms(500);
+
+        count_minutes++;
+        if (count_minutes == 10 && cobra.tamanho < 8){
+           cobra.velocidade = cobra.velocidade - 50;
+           aumentar_cobra(&cobra);
+           count_minutes = 0;
+        }
+
+        verifica_limites(&cobra, &count_minutes);
+
+    }
+
+    return 0;
 }
