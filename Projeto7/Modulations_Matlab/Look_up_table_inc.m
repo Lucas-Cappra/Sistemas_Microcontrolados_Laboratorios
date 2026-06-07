849093a658d51ca9ac99323169110edd2767ea1b
Pedro <105248310+PedroDS4@users.noreply.github.com>
2026-06-07 09:48:27 -0300

    Update Look_up_table_inc.m

--- Projeto7/Modulations_Matlab/Look_up_table_inc.m
+++ Projeto7/Modulations_Matlab/Look_up_table_inc.m
@@ -11,27 +11,35 @@ fase = linspace(0, 2*pi-0.001, N);
 % Cria a senoide centrada em 127.5, variando de 0 a 255
 senoide = 127.5 + 127.5 * sin(fase);
 
-% Arredonda para valores inteiros
-% senoide = uint8(senoide);
 senoide  = round(senoide);
 
-% Frequencia
-f = 120;
-
-% Conversão para amostras de fase
-ponteiro_fase = round(f*N/F_s);
-
-% senoide = 5*senoide/max(senoide);
-
-senoide = senoide(1:ponteiro_fase:end);
-
-N_new = length(senoide);
-t = (0:N_new-1)*T_s;
+% Conversão D/A pra saída
+senoide = 5*senoide/max(senoide);
 
 
-% Agora você tem exatamente 256 valores de 0 a 255
-figure()
-plot(t, senoide,'color', 'red','linewidth',4 );
-xlabel('t(s)');
-ylabel('Amplitude');
-grid on
+freq_desejada = 10; % Frequência da portadora em Hz
+
+% deve completar (F_s) ciclos.
+% Fórmula: Inc = (f_desejada * 2^32) / F_s
+inc_32 = uint32((freq_desejada * 2^32) / F_s);
+
+num_amostras = 1000; %
+acumulador = uint32(0);
+saida_seno = zeros(1, num_amostras);
+
+for i = 1:num_amostras
+    % 1. O acumulador avança (simulando o estouro natural do uint32)
+    acumulador = acumulador + inc_32; 
+    
+    % 2. 8 bits mais significativos 
+    indice = bitshift(acumulador, -24) + 1; 
+    
+    % 3. Consulta a LUT
+    saida_seno(i) = senoide(indice);
+end
+
+% Plotagem dos gra´ficos
+figure;
+plot(saida_seno, 'LineWidth', 2);
+title(['Sinal Gerado - Freq: ' num2str(freq_desejada) 'Hz']);
+grid on;
